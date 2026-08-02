de587b0cc841368fa1a68bd55e981e731164fce6
Aryan Yadav <ydvaaryan21@gmail.com>
2026-08-02 11:53:45 +0530

    Time: 5 ms (52.73%), Space: 46.6 MB (62.43%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.java
+++ 0006-zigzag-conversion/0006-zigzag-conversion.java
@@ -1,5 +1,7 @@
 class Solution {
     public String convert(String s, int numRows) {
+        
+        // single row check 
         if(numRows == 1 || numRows > s.length()){
             return s;
 
@@ -7,37 +9,37 @@ class Solution {
 
         StringBuilder[] rows = new StringBuilder[numRows];
 
+        // iterate each row
         for(int i=0 ; i < numRows; i++){
             rows[i]  = new StringBuilder() ;
 
         }
 
-        int currentRow = 0;
-        boolean isGoingDown = false;
+        int currentRow = 0;  // let's take variable to check 
+        boolean isGoingDown = false;  // is goingDown 
 
+        // iterate each Character of Array
         for(char c: s.toCharArray()){
-            rows[currentRow].append(c);
+            rows[currentRow].append(c);  // append each elements to last of array
 
+             // going down depends on condition - last numRows - 
              if(currentRow == 0 || currentRow == numRows - 1){
                 isGoingDown = !isGoingDown; // becomes true
 
             }
 
+            // increment & decrement
             currentRow += isGoingDown ? 1 : -1 ;
 
         }
 
+        // to store into StringBuilder.
         StringBuilder res = new StringBuilder();
         for(StringBuilder row : rows){
             res.append(row);
         }
 
         return res.toString();
-
-        
-        
-
-    
         
     }
 }
