c522b481ce28fecca8b50fe2b3d8f25fe9f7e96e
Aryan Yadav <ydvaaryan21@gmail.com>
2026-08-16 20:52:00 +0530

    Time: 1 ms (99.99%), Space: 43.2 MB (87.94%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -2,6 +2,11 @@
  key difficulty is understanding how * works.
  Dynamic Programming (DP).
 
+ ' . ' is any charcter
+ ' * ' Take the character immediately before *  , zero or more times.
+
+ “Can the remaining part of s, starting from i, match the remaining part of p, starting from j?”
+
 dp[i][j] = whether s.substring(i) matches p.substring(j).
 
 At every (i, j):
@@ -13,6 +18,14 @@ At every (i, j):
     Skip x* completely → dp[i][j + 2]
     Use x* to match one character → dp[i + 1][j]     
 
+                a*
+               /  \
+              /    \
+          SKIP      USE
+            |        |
+            ↓        ↓
+         j + 2    i + 1, j
+
 
 */
 
@@ -21,45 +34,51 @@ class Solution{
         int m = s.length();
         int n = p.length();
 
-        Boolean[][] dp = new Boolean[m + 1][n + 1];  
-        return solve(0 , 0 , s , p , dp);
+        // Boolean - null , true , false 
+
+        Boolean[][] dp = new Boolean[m + 1][n + 1];  // represent the empty remaining string.
+
+        return solve(0 , 0 , s , p , dp);  // 0 → starting position in s (i=0), 0 → starting position in p (j=0), s → actual string, p → pattern, dp → DP memory table
 
     }
 
     private boolean solve(int i , int j , String s , String p , Boolean[][] dp ){
 
-        // Pattern completely consumed
+        // “Have we finished reading the pattern?”
         if( j == p.length()){
-            return i == s.length();
+            return i == s.length();  // “Is the string also finished?”
 
         }
 
+        // Checking our DP table 
         if(dp[i][j] != null){
             return dp[i][j];
 
         }
 
-        // checks if current character matches
+        // “Can the current character of s match the current character of p?”
         boolean firstMatch =  i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
-        boolean result ; // true
+
+        boolean result ; // true or false - depending on whether the remaining string and pattern match.
 
         // Next charcter is '*'
         if( j + 1 < p.length() && p.charAt( j + 1) == '*'){
 
-            // Option 1: Ignore "x*"
+            // Option 1: Ignore "x*" completely and continue with after it.
             boolean skip = solve(i, j + 2, s, p, dp);
-              // Option 2: Use '*' to match current character
+
+            // Option 2: Use '*' to match current character
             boolean use = firstMatch &&
-                    solve(i + 1, j, s, p, dp);
+                    solve(i + 1, j, s, p, dp);  //  Why don’t we move j? Because * can use a again.
                     
                     result = skip || use;
-        }else{
+        } else {
             // Normal character or ' . '
             result = firstMatch && solve(i + 1, j + 1, s, p, dp);
         }
 
-        dp[i][j] = result;
-        return result;
+        dp[i][j] = result;  // Next time we reach like (1,2), we don’t have to calculate it again.
+        return result; // true or false
         
     }
 }
