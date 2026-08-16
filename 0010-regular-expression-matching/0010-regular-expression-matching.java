3697101344f6be59e36d0191febba96e9f4d9fb1
Aryan Yadav <ydvaaryan21@gmail.com>
2026-08-16 20:15:56 +0530

    Time: 1 ms (99.99%), Space: 43.7 MB (22.71%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,52 +1,65 @@
-class Solution {
-    public boolean isMatch(String s, String p) {
+/*
+ key difficulty is understanding how * works.
+ Dynamic Programming (DP).
 
+dp[i][j] = whether s.substring(i) matches p.substring(j).
+
+At every (i, j):
+1. Check whether the current characters match:
+     s[i] == p[j]
+     or p[j] == '.'
+
+2. If the next pattern character is *, we have two choices:
+    Skip x* completely → dp[i][j + 2]
+    Use x* to match one character → dp[i + 1][j]     
+
+
+*/
+
+class Solution{
+    public boolean isMatch(String s , String p){
         int m = s.length();
         int n = p.length();
 
-        Boolean[][] dp = new Boolean[m + 1][n + 1];
+        Boolean[][] dp = new Boolean[m + 1][n + 1];  
+        return solve(0 , 0 , s , p , dp);
 
-        return solve(0, 0, s, p, dp);
     }
 
-    private boolean solve(int i, int j, String s, String p, Boolean[][] dp) {
+    private boolean solve(int i , int j , String s , String p , Boolean[][] dp ){
 
         // Pattern completely consumed
-        if (j == p.length()) {
+        if( j == p.length()){
             return i == s.length();
+
         }
 
-        if (dp[i][j] != null) {
+        if(dp[i][j] != null){
             return dp[i][j];
+
         }
 
-        // Check if current characters match
-        boolean firstMatch =
-                i < s.length() &&
-                (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        // checks if current character matches
+        boolean firstMatch =  i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.');
+        boolean result ; // true
 
-        boolean result;
-
-        // Next character is '*'
-        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+        // Next charcter is '*'
+        if( j + 1 < p.length() && p.charAt( j + 1) == '*'){
 
             // Option 1: Ignore "x*"
             boolean skip = solve(i, j + 2, s, p, dp);
-
-            // Option 2: Use '*' to match current character
+              // Option 2: Use '*' to match current character
             boolean use = firstMatch &&
                     solve(i + 1, j, s, p, dp);
-
-            result = skip || use;
-
-        } else {
-
-            // Normal character or '.'
-            result = firstMatch &&
-                    solve(i + 1, j + 1, s, p, dp);
+                    
+                    result = skip || use;
+        }else{
+            // Normal character or ' . '
+            result = firstMatch && solve(i + 1, j + 1, s, p, dp);
         }
 
         dp[i][j] = result;
         return result;
+        
     }
 }
