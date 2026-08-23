63147cd364155c1623f8881f0a01f7785ed388e8
Aryan Yadav <ydvaaryan21@gmail.com>
2026-08-23 22:35:29 +0530

    Time: 2 ms (93.63%), Space: 72.4 MB (6.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,35 +1,55 @@
 /** Main logic is:  Product sum except iteslt = Product Right * Product Left
 
+                Product Except Self
+                         │
+                         ↓
+             ┌─────────────────────┐
+             │                     │
+             ↓                     ↓
+        LEFT PRODUCT          RIGHT PRODUCT
+             │                     │
+             └──────────┬──────────┘
+                        ↓
+                    MULTIPLY
+                        ↓
+                     ANSWER
 
 
-
-
+    Time Complexity -> O(n). both first and Second loops are O(n)
+    Space Complexity -> O(1) , The output array is required, and your algorithm uses only two extra variables:                
 
  */
 
-
 class Solution {
-    public int[] productExceptSelf(int[] nums) {
 
-        int n = nums.length;
-        int[] sum = new int[n];
-        int leftProd = 1;
-        int rightProd = 1;
+    public int[] productExceptSelf(int[] nums) {   
 
+        int n = nums.length;  // n = 4 
+
+        int[] ans = new int[n]; // Initially Java gives integer arrays the default value 0. 
+
+        int leftProd = 1;       // Product of all elements to the LEFT of the current index. ,, 1 acts as the neutral element for multiplication.
+
+        int rightProd = 1;      // Product of all elements to the RIGHT of the current index.
+
+        // from right to left , array index starts from 0 so (n-1), decreasing
         for(int i = n-1; i >= 0; i-- ){
-            sum[i] = rightProd;
+
+            ans[i] = rightProd; // Put the product of everything to the RIGHT of i into ans[i].
             rightProd = rightProd * nums[i];
         
         }
 
         for(int i=0; i<n; i++){
-            sum[i] = sum[i] * leftProd;
-            leftProd = leftProd * nums[i];
+            ans[i] = ans[i] * leftProd; // it already containes rightproduct , so only multiply it into leftproduct
+            leftProd = leftProd * nums[i];  // update leftproduct
 
         }
 
-        return sum;
+        return ans;
 
         
     }
 }
+
+// algorithm also works beautifully with zero
